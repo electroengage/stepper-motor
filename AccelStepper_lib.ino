5ef6a6afe26992af1c7cb09a34f6b580d45bb5f4
Md. Tamim <107707836+mdtamimcpp@users.noreply.github.com>
2023-05-12 20:19:22 +0600

    Create AccelStepper_lib.ino

---
+++ AccelStepper_lib.ino
@@ -0,0 +1,19 @@
+#include <AccelStepper.h>
+
+// Define the AccelStepper interface type:
+#define MotorInterfaceType 4
+
+// Create a new instance of the AccelStepper class:
+AccelStepper stepper = AccelStepper(MotorInterfaceType, 8, 9, 10, 11);
+
+void setup() {
+  // Set the maximum speed in steps per second:
+  stepper.setMaxSpeed(1000);
+}
+
+void loop() {
+  // Set the speed of the motor in steps per second:
+  stepper.setSpeed(500);
+  // Step the motor with constant speed as set by setSpeed():
+  stepper.runSpeed();
+}
